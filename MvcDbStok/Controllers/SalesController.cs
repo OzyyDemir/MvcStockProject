f464ab8d18ea5c6e516337d287179c274c25df5a
Oğuzhan DEMİR <oguzhandmr98@gmail.com>
2021-09-20 15:21:15 +0300

    Commit 5

---
+++ MvcDbStok/Controllers/SalesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcDbStok.Models.Entity;
+
+namespace MvcDbStok.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult YeniSatis()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniSatis(SATISLAR satis)
+        {
+            db.SATISLAR.Add(satis);
+            db.SaveChanges();
+            return View("Index");
+        }
+    }
+}
